38dca103b233ade6658f034c963866f45ddbc873
Kevin <kevinrisky18@gmail.com>
2024-11-09 19:09:15 +0700

    fix

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+const PORT = process.env.PORT || 3000;
 
 const express = require("express");
 const app = express();
-const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/coba", (req, res) => {
   res.send("Hello, Kevin");
+});
+
+app.post("/register", async (req, res) => {
+  const { email, name, password } = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        password,
+      },
+    });
+    res.status(200).json({ message: "berhasil brok" });
+  } catch (error) {
+    res.status(400).json({ message: "gagal brok" });
+  }
 });
 
 app.listen(PORT, () => {
